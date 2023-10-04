8b5fb9e367e450c05018cdb5ab7f55db836d3abe
Trent <rexzhang68@gmail.com>
2023-10-04 00:18:55 -0500

    Hide sensitive data

--- Frontend/beatmap/src/app/test/page.tsx
+++ Frontend/beatmap/src/app/test/page.tsx
@@ -1,6 +1,5 @@
 // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
-const token =
-  "BQDfB9ouOOzsEvREg75gC3Xe005i-N8hYrrJV7ATJtvwqoeU0pIzxJM4k4KOpGXcdlbHUsZGfjEKh96MLgFDyv55qKLBXYqA4lBD8oahPDp8GvIHoFKxgAd8R236YUDF_HDZJ92BlCBCqb1U8ZIWQ1rFmq7bpOk7eGPL9TfSl2RiA6B3LvJGG6GL8drkmOyEHCGfRylref9KeJkBJHNEe2svlooo-Dr-IzyU0BtwZTiyaUltGuLiZMiEWYBljHdQR-U";
+
 async function fetchWebApi(endpoint: string, method: string) {
   const res = await fetch(`https://api.spotify.com/${endpoint}`, {
     headers: {
