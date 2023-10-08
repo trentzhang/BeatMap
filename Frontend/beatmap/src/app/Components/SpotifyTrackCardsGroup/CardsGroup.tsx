a748a6facb94b39f74eae5db7cb6bc27cd760518
Trent <rexzhang68@gmail.com>
2023-10-08 18:20:21 -0500

    fix mongodb not closed problem

--- Frontend/beatmap/src/app/Components/SpotifyTrackCardsGroup/CardsGroup.tsx
+++ Frontend/beatmap/src/app/Components/SpotifyTrackCardsGroup/CardsGroup.tsx
@@ -10,8 +10,8 @@ export default async function MusicComponent({
   const topTracks = await getTopTracks(spotifyToken);
   //   const track = topTracks[0];
   return (
-    <div className="grid place-items-center min-h-screen  p-5">
-      <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold text-gray-200 mb-5">
+    <div className="grid place-items-center min-h-screen  p-5 bg-gradient-to-b from-blue-600 to-slate-500 rounded-lg mx-5">
+      <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold text-gray-300  mb-5">
         Your Top 50 Songs
       </h1>
       <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
@@ -37,6 +37,25 @@ export default async function MusicComponent({
           )
         )}
       </section>
+      {/* Collapse button */}
+      <button className="my-3">
+        <svg
+          className="w-10 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 12"
+          preserveAspectRatio="none"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 7 4 4 4-4M1 1l4 4 4-4"
+          />
+        </svg>
+      </button>
     </div>
   );
 }
