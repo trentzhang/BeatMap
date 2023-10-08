00de48724fb7d1a49e053def00b9a6f32ecc6a07
Trent <rexzhang68@gmail.com>
2023-10-08 16:55:08 -0500

    fixed play stop button

--- Frontend/beatmap/src/app/Components/SpotifyTrackCardsGroup/PlayButton.tsx
+++ Frontend/beatmap/src/app/Components/SpotifyTrackCardsGroup/PlayButton.tsx
@@ -1,34 +1,56 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+interface AudioPlayerProps {
+  audioUrl: string;
+}
 
 const PlayButton = ({ preview_url }: { preview_url: string }) => {
-  let audio = typeof Audio !== "undefined" ? new Audio(preview_url) : null;
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const toggleAudio = () => {
+    const audio = audioRef.current;
 
-  const start = () => {
     if (audio) {
-      if (audio.paused === true) {
-        audio.play();
-      } else {
+      if (isPlaying) {
         audio.pause();
+      } else {
+        audio.play();
       }
     }
+
+    setIsPlaying(!isPlaying);
   };
   return (
     <button
       className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition"
-      onClick={start}
+      onClick={toggleAudio}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="40"
-        height="40"
-        fill="currentColor"
-        className="bi bi-play-circle-fill"
-        viewBox="0 0 16 16"
-      >
-        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
-      </svg>
+      {isPlaying ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          fill="currentColor"
+          className="bi bi-stop-circle-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.5 5A1.5 1.5 0 0 0 5 6.5v3A1.5 1.5 0 0 0 6.5 11h3A1.5 1.5 0 0 0 11 9.5v-3A1.5 1.5 0 0 0 9.5 5h-3z" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          fill="currentColor"
+          className="bi bi-play-circle-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
+        </svg>
+      )}
+      <audio ref={audioRef} src={preview_url} />
     </button>
   );
 };
