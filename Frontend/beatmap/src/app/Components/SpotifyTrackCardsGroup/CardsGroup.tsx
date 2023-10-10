11de0ccd425e0830fc9c514697963c8daa51cb40
Trent <rexzhang68@gmail.com>
2023-10-10 01:29:57 -0500

    add animation

--- Frontend/beatmap/src/app/Components/SpotifyTrackCardsGroup/CardsGroup.tsx
+++ Frontend/beatmap/src/app/Components/SpotifyTrackCardsGroup/CardsGroup.tsx
@@ -1,28 +1,33 @@
 "use client";
-import { topTracksDefault } from "@/app/Utils/DefaultVariables";
-import { SpotifyCard } from "./TrackCard";
-import { Accordion, AccordionItem } from "@nextui-org/react";
 import { Card, CardBody } from "@nextui-org/react";
 import { useUserContext } from "../SelectedUserContext";
-import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { SpotifyCard } from "./TrackCard";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function MusicComponent() {
   const { selectedUser, setSelectedUser } = useUserContext();
   const topTracks = selectedUser?.topTracks || null;
   const MyCard = (
-    <Card className="mx-5 min-h-[40em] bg-gradient-to-b from-blue-600 to-slate-500">
-      <CardBody>
-        <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold text-gray-300  mb-5">
-          Top 50 Songs
-        </h1>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
-          {topTracks?.items?.map((topTrack) => (
-            <SpotifyCard topTrack={topTrack} key={topTrack.id}></SpotifyCard>
-          ))}
-        </div>
-      </CardBody>
-    </Card>
+    <motion.div
+      initial={{ opacity: 0, y: "5%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "5%" }}
+      transition={{ duration: 0.5 }}
+      key={selectedUser?._id}
+    >
+      <Card className="mx-5 min-h-[40em] bg-gradient-to-b from-blue-600 to-slate-500">
+        <CardBody>
+          <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold text-gray-300  mb-5">
+            Top 50 Songs
+          </h1>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+            {topTracks?.items?.map((topTrack) => (
+              <SpotifyCard topTrack={topTrack} key={topTrack.id}></SpotifyCard>
+            ))}
+          </div>
+        </CardBody>
+      </Card>
+    </motion.div>
   );
   const NothingHereCard = (
     <Card className="mx-5 min-h-[20em]  bg-gradient-to-b from-blue-600 to-sky-600">
@@ -34,11 +39,9 @@ export default function MusicComponent() {
     </Card>
   );
 
-  const [cardContent, setCardContent] = useState("Card 1");
-
-  const changeCardContent = () => {
-    // Change the content for demonstration
-    setCardContent(`Card ${Math.floor(Math.random() * 100)}`);
-  };
-  return selectedUser ? MyCard : NothingHereCard;
+  return (
+    <AnimatePresence mode="wait">
+      {selectedUser ? MyCard : null}
+    </AnimatePresence>
+  );
 }
